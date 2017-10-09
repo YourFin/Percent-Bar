8f42bc628523a65b92f4b8236c174dbe7568f40f
YourFin <d.junkpobox+github@gmail.com>
2017-10-09 11:56:54 -0500

    Basic bar working!

--- percent-bar.rb
+++ percent-bar.rb
@@ -3,6 +3,7 @@ require 'rubygems'
 require 'optparse'
 require 'pathname'
 require 'digest'
+require 'color'
 
 $options = {}
 
@@ -17,9 +18,26 @@ parser = OptionParser.new do |opts|
     $options[:posX] = pos[0]
     $options[:posY] = pos[1]
   end
-  opts.on("--size width,height", Array, "Size of window on screen") do |size|
-    $options[:width] = size[0]
-    $options[:height] = size[1]
+  opts.on("-f", "--foreground STRING or HEX", String, "Color of bar") do |color|
+    $options[:foreground] = Color::RGB.extract_colors(color)[0]
+  end
+  opts.on('-b', '--background STRING or HEX', String, "Color of background") do |color|
+    $options[:background] = Color::RGB.extract_colors(color)[0]
+  end 
+  opts.on("-W", "--width WIDTH", Integer, "Width of window on screen") do |width|
+    $options[:width] = width
+  end
+  opts.on("-H", "--height HEIGHT", Integer, "Height of window on screen") do |height|
+    $options[:height] = height
+  end
+  #opts.on('-i', '--icon PATH', URI, "Path to icon.") do |icon|
+  #  $options[:icon] = icon
+  #end
+  opts.on('-c', '--caption', 'Caption for the window') do |caption|
+    $options[:caption] = caption
+  end
+  opts.on("--description STRING", String, "Description for bar. Should be unique if provided.") do |desc|
+    $options[:description] = desc
   end
   opts.on('-h', '--help', 'Display this screen') do
     puts opts
@@ -44,15 +62,12 @@ rescue TypeError
 end
 
 ##Hash all the command line options to create
-puts $options.inspect
 #a unique identifier
 hash = Digest::MD5.new.update($options.inspect).base64digest
 pipePath = Pathname.new('/tmp/percent-bar-' + hash)
 
-# Default arguments
-$options[:delay] = 2 if not $options.has_key? :delay
 
-## Create Pipe
+## Create Pipe or write to it
 if pipePath.pipe?
   pipePath.open('w') { |file| file.write(ARGV[0])}
   exit
@@ -62,13 +77,40 @@ else system('mkfifo ' + pipePath.to_s) end
 require 'gosu'
 require 'thread'
 
+# Default arguments
+$options[:delay] = 2 if not $options.has_key? :delay
+$options[:height] = 150 if not $options.has_key? :height
+$options[:width] = $options[:height] / 5 if not $options.has_key? :width
+$options[:background] = Color::RGB.by_name("Black") if not $options.has_key? :background
+$options[:foreground] = Color::RGB.by_name("White") if not $options.has_key? :foreground
+
+def colorToGosu(rgb)
+  return Gosu::Color.argb(255, rgb.red, rgb.green, rgb.blue)
+end
+
+$options[:background] = colorToGosu($options[:background])
+$options[:foreground] = colorToGosu($options[:foreground])
+
+$barwidth = $options[:width] * 3 / 5
+$barX = $options[:width] / 5
+$padding = $barX
+
 $mutex = Mutex.new
 $percentMutex = Mutex.new
 
+
+def bound(val)
+  return [[val, 0].max, 100].min / 100
+end
+
+def myheight(val)
+  return ($options[:height] - ($padding * 2)) * bound(val)
+end
+
 class BarWindow < Gosu::Window
   def initialize
-    super 640, 400
-    self.caption = "Tutorial Game"
+    super $options[:width], $options[:height]
+    self.caption = "Percent bar"
     @font = Gosu::Font.new(20)
   end
 
@@ -77,9 +119,11 @@ class BarWindow < Gosu::Window
   end
   
   def draw
-    Gosu.draw_rect(200, 200, 100, 100, Gosu::Color::WHITE)
     $percentMutex.synchronize do
-        Gosu.draw_rect(225, 210, 50, $percent.to_i, Gosu::Color::BLACK)
+      Gosu.draw_rect(0,0,$options[:width],$options[:height],$options[:background])
+      Gosu.draw_rect($barX.to_f,
+                     $options[:height].to_f - $padding - myheight($percent),
+                     $barwidth.to_f, myheight($percent.to_f), $options[:foreground])
     end
   end
 
@@ -88,8 +132,6 @@ class BarWindow < Gosu::Window
   end
 end
 
-puts "hello"
-
 #show window and fork to new thread
 $windowThread = Thread.new {BarWindow.new.show}
 
@@ -97,7 +139,6 @@ $windowThread = Thread.new {BarWindow.new.show}
 at_exit { system("rm -f " + pipePath.to_s)}
 
 def timer
-  puts "timer"
   sleep $options[:delay]
   $mutex.synchronize {
     $windowThread.kill
@@ -107,15 +148,18 @@ end
 
 $timerThread = Thread.new {timer()}
 
-loop do
-  puts "loop"
-  input = pipePath.open('r')
-  a = input.gets
-  $percentMutex.synchronize do
-    $percent = a
-  end
+loopThread = Thread.new do
+  loop do
+    input = pipePath.open('r')
+    a = input.gets
+    $percentMutex.synchronize do
+      $percent = a.to_f
+    end
 
-  $timerThread.kill
-  $timerThread = Thread.new {timer()}
-  input.close
+    $timerThread.kill
+    $timerThread = Thread.new {timer()}
+    input.close
+  end
 end
+
+$windowThread.join
